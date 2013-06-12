47f4b4c1d7cebaa9588b6e7ce36ee2e66fc5b1ab
KoichiKuraya <kurayakouichi@kkuraya.local>
2013-06-12 19:13:00 +0900

    add ruby.rb roll

---
+++ roles/ruby.rb
@@ -0,0 +1,19 @@
+name "ruby"
+description "Configure Ruby environment"
+run_list(
+  # TODO support ubuntu
+  "recipe[yum::epel]",
+  "recipe[ruby_build]",
+  "recipe[rbenv::system]"
+)
+default_attributes({
+  "rbenv" => {
+    "rubies" => [ "1.9.3-p392" ],
+    "global" => "1.9.3-p392",
+    "gems" => {
+      "1.9.3-p392" => [
+        { "name" => "bundler", "version" => "1.3.5" }
+      ]
+    }
+  }
+})
