a6b98c873ff8937e4a6441128456cb96c824b7ee
KoichiKuraya <kurayakouichi@kkuraya.local>
2013-09-26 12:06:30 +0900

    add redmine role

---
+++ roles/redmine.rb
@@ -0,0 +1,24 @@
+name "redmine"
+description "Redmine"
+run_list(
+  "role[ruby]",
+  "recipe[redmine]",
+  "recipe[redmine::backup]"
+)
+default_attributes({
+  "rbenv" => {
+    "gems" => {
+      "1.9.3-p392" => [
+        { "name" => "rails",   "version" => "3.2.13" },
+        { "name" => "unicorn", "version" => "4.6.2"  }
+      ]
+    }
+  },
+  "mysql" => {
+    "tunable" => {
+      "max_connections" => "100",
+      "innodb_buffer_pool_size" => "512MB",
+      "innodb_log_file_size" => "128MB"
+    }
+  }
+})
