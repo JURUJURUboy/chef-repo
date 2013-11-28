7cbc00980e6736319f3312b75620bf00d3e739ab
KoichiKuraya <everything.maked.nebaneba@gmail.com>
2013-11-28 19:15:59 +0900

    global version up

--- roles/ruby.rb
+++ roles/ruby.rb
@@ -9,9 +9,9 @@ run_list(
 default_attributes({
   "rbenv" => {
     "rubies" => [ "2.0.0-p0" ],
-    "global" => "1.9.3-p392",
+    "global" => "2.0.0-p0",
     "gems" => {
-      "1.9.3-p392" => [
+      "2.0.0-p0" => [
         { "name" => "bundler", "version" => "1.3.5" },
       ]
     }
